eb8e1a234b73cc51c0e3e88daa0c30df1589015e
yashavantpatil <165567757+yashavantpatil@users.noreply.github.com>
2025-12-30 12:38:16 +0530

    Add files via upload

---
+++ Diabetes_Prediction.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import joblib
+import streamlit as st
+
+model = joblib.load(open("logistic_regression_model for Diabetes", "rb"))
+
+st.title("Diabetes Prediction App")
+
+preg = st.number_input("Pregnancies", 0, 20)
+glu = st.number_input("Glucose", 0, 200)
+bp = st.number_input("Blood Pressure", 0, 150)
+skin = st.number_input("Skin Thickness", 0, 100)
+insulin = st.number_input("Insulin", 0, 900)
+bmi = st.number_input("BMI", 0.0, 70.0)
+dpf = st.number_input("Diabetes Pedigree Function", 0.0, 3.0)
+age = st.number_input("Age", 1, 100)
+
+input_data = np.array([[preg, glu, bp, skin, insulin, bmi, dpf, age]])
+
+if st.button("Predict"):
+    result = model.predict(input_data)
+    st.success("Diabetic" if result[0] == 1 else "Not Diabetic")
